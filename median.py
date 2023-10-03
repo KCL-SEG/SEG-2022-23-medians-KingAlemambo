3245d9de9a1f4aa5b46d6a6fb3a02c0c920a9d95
alemambo <alexandros.mathios@outlook.com>
2023-10-03 14:22:07 +0100

    median exercise

--- median.py
+++ median.py
@@ -1,24 +1,23 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def findMedian(numbers):
-    print("enter median method   ")
-    numbers.sort()
-    n=numbers.length()/2
-    if numbers.length() % 2 == 0:
-        median=(numbers[n] + numbers[n+1])/2
-    else:
-        median=numbers[n]
-    print("median method finished")
-    return median
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        findMedian(numbers)
+        numbers.sort()
+        n=len(numbers)//2
+        median =0
+        if len(numbers) % 2 == 0:
+            median=(numbers[n] + numbers[n+1])//2
+        else:
+            median=numbers[n]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median)
+
+
+
